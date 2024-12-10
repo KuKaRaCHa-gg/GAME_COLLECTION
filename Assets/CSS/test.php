ef89f28c2ee8ae1e7bcdfa4a0f34b4f7ac67a34d
MaxouHunter11 <maxbanuls@laposte.net>
2024-12-10 11:32:54 +0100

    Fix assets

---
+++ Assets/CSS/test.php
@@ -0,0 +1,41 @@
+<!DOCTYPE>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="FormulaireConnexion.css">
+    <link rel="stylesheet" type="text/css" href="Backround.css">
+</head>
+<body>
+    <h1>
+        Test
+    </h1>
+<form>
+    <h1>Se connecter a Game Collection</h1>
+    <label for="username">Username</label>
+    <input type="text" name="username">
+    <label for="password">Password</label>
+    <input type="password" name="password">
+    <button type="submit">Login</button>
+    <a href="signup.php">Sign Up</a>
+</form>
+    <form>
+        <h1>Test</h1>
+        <label for="username">Nom</label>
+        <input type="text" name="username">
+        <label for="username">Nom</label>
+        <input type="text" name="username">
+        <label for="password">Mot de Passe</label>
+        <input type="password" name="password">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <button type="submit">Connexion</button>
+        <button type="submit">AAAAAAAAAAAAAAA</button>
+        <button type="submit">GRDFGFD</button>
+        <a href="signup.php">Inscription</a>
+    </form>
+</body>
