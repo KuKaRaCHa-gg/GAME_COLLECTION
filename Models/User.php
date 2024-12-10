5bf2eedaa430581acf31d9a3a9e1d3f067c1ab4f
MattisGoom64 <schaefermattis64@gmail.com>
2024-12-10 11:27:48 +0100

    TEST

--- Models/User.php
+++ Models/User.php
@@ -1,2 +1,26 @@
 <?php
 
+
+
+function getUser()
+{
+    require 'vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    try {
+        $pdo = new PDO(
+            "mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASSWORD']
+        );
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Erreur de connexion : " . $e->getMessage());
+    }
+
+    $query = $pdo->prepare("SELECT * FROM UTILISATEUR");
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+}
