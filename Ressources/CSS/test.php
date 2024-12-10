a93d984695cbe9e9802d67142a77b00db3a08dde
MaxouHunter11 <maxbanuls@laposte.net>
2024-12-10 11:25:43 +0100

    Fix formulaire css + nouvelle branche

--- Ressources/CSS/test.php
+++ Ressources/CSS/test.php
@@ -17,4 +17,25 @@
     <button type="submit">Login</button>
     <a href="signup.php">Sign Up</a>
 </form>
+    <form>
+        <h1>Test</h1>
+        <label for="username">Nom</label>
+        <input type="text" name="username">
+        <label for="username">Nom</label>
+        <input type="text" name="username">
+        <label for="password">Mot de Passe</label>
+        <input type="password" name="password">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <label for="a">Bonjour</label>
+        <input type="checkbox" name="a">
+        <button type="submit">Connexion</button>
+        <button type="submit">AAAAAAAAAAAAAAA</button>
+        <button type="submit">GRDFGFD</button>
+        <a href="signup.php">Inscription</a>
+    </form>
 </body>
