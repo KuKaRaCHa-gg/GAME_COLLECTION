28e88fa7f3877923c5b2659e3c9e8d4c5a4d7df1
MattisGoom64 <schaefermattis64@gmail.com>
2024-12-16 17:34:45 +0100

    getUser update

--- Models/User.php
+++ Models/User.php
@@ -10,16 +10,23 @@ function getUser($pdo, $id)
     $row = $query->fetch(PDO::FETCH_ASSOC);
     echo '<br>';
     echo '<form action="" method="post">';
+
     echo '<label for="nom_user">Nom</label><br>';
     echo '<input type="text" name="nom_user" value="' . htmlspecialchars($row['nom_user']) . '"><br>';
+
     echo '<label for="pren_user">Prénom</label><br>';
     echo '<input type="text" name="pren_user" value="' . htmlspecialchars($row['pren_user']) . '"><br>';
+
     echo '<label for="mail_user">Email</label><br>';
     echo '<input type="text" name="mail_user" value="' . htmlspecialchars($row['mail_user']) . '"><br>'; 
+
     echo '<label for="mdp_user">Mot de passe</label><br>';
     echo '<input type="password" name="mdp_user" value=""><br>';
     echo '<label for="mdp_user">Confirmer le mot de passe</label><br>';
     echo '<input type="password" name="mdp_user" value=""><br>';
-    echo '<input type="submit" name="submit" value="Modifier">';
+
+    echo '<input type="submit" name="submit" value="Modifier"><br>';
+    echo '<input type="submit" name="submit" value="Supprimer mon compte"><br>';
+    echo '<input type="submit" name="submit" value="Se déconnecter"><br>';
     echo '</form>';
 }
