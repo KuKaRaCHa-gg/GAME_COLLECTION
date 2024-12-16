cecdbb2341de2ff395b7351351a48f4778fcbec2
MaxouHunter11 <maxbanuls@laposte.net>
2024-12-16 16:13:26 +0100

    Modif navbar

--- Views/NavBar.php
+++ Views/NavBar.php
@@ -6,12 +6,24 @@
 </head>
 <body>
 <div id="navabarFrame">
-    <img id="logo" src="../Assets/Images/Logo.png" alt="Logo" />
+    <img id="logo" src="../Assets/Images/Logo.png" alt="Logo"/>
     <div id="buttonStorage">
-        <button type="button" id="buttonHome">MA BIBLIOTHÈQUE</button>
-        <button type="button" id="buttonCollection">AJOUTER UN JEU</button>
-        <button type="button" id="buttonSearch">CLASSEMENT</button>
-        <button type="button" id="buttonProfile">PROFIL</button>
+        <button type="button" id="buttonHome">
+            MA BIBLIOTHÈQUE
+            <img src="./../Assets/Images/livres.png" alt="livres" id="livres" class="navimages">
+        </button>
+        <button type="button" id="buttonCollection">
+            AJOUTER UN JEU
+            <img src="./../Assets/Images/manette.png" alt="manette" id="manette" class="navimages">
+        </button>
+        <button type="button" id="buttonSearch">
+            CLASSEMENT
+            <img src="./../Assets/Images/couronne.png" alt="couronne" id="couronne" class="navimages">
+        </button>
+        <button type="button" id="buttonProfile">
+            PROFIL
+            <img src="./../Assets/Images/profil.png" alt="profil" id="profil" class="navimages">
+        </button>
     </div>
 </div>
 </body>
