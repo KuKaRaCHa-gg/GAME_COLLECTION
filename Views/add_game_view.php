49bba188a0460016bda6e329f64d08ee96627354
KuKaRaCHa-gg <daniilpub@gmail.com>
2025-01-02 14:09:16 +0100

    add_game_view.php start

--- Views/add_game_view.php
+++ Views/add_game_view.php
@@ -1,23 +1,49 @@
 <?php include 'header.php'; ?>
 <div class="add-game-container">
     <h1>Ajouter un jeu à sa bibliothèque</h1>
+    <p>Vérifiez si le jeu que vous souhaitez ajouter existe déjà dans la base de données. Sinon, ajoutez-le !</p>
+
     <form action="index.php?action=add_game" method="POST">
-        <label for="game_name">Nom du jeu :</label>
-        <input type="text" id="game_name" name="game_name" required>
+        <label for="search_game">Rechercher un jeu :</label>
+        <input type="text" id="search_game" name="search_game" placeholder="Nom du jeu">
+        <button type="submit" name="action" value="search_game">Rechercher</button>
+    </form>
 
-        <label for="publisher">Éditeur :</label>
-        <input type="text" id="publisher" name="publisher" required>
+    <hr>
 
-        <label for="release_date">Date de sortie :</label>
-        <input type="date" id="release_date" name="release_date" required>
+    <h2>Ajouter un nouveau jeu</h2>
+    <form action="index.php?action=add_game" method="POST">
+        <label for="nom_game">Nom du jeu :</label>
+        <input type="text" id="nom_game" name="nom_game" placeholder="Nom du jeu" required>
 
-        <label for="cover_url">URL de la couverture :</label>
-        <input type="url" id="cover_url" name="cover_url" required>
+        <label for="edit_game">Éditeur du jeu :</label>
+        <input type="text" id="edit_game" name="edit_game" placeholder="Éditeur du jeu" required>
 
-        <label for="hours_played">Temps joué (heures) :</label>
-        <input type="number" id="hours_played" name="hours_played" min="0" required>
+        <label for="release_game">Sortie du jeu :</label>
+        <input type="date" id="release_game" name="release_game" required>
 
-        <button type="submit">Ajouter à la bibliothèque</button>
+        <label>Plateformes :</label>
+        <div>
+            <input type="checkbox" id="playstation" name="plateformes[]" value="Playstation">
+            <label for="playstation">Playstation</label>
+
+            <input type="checkbox" id="xbox" name="plateformes[]" value="Xbox">
+            <label for="xbox">Xbox</label>
+
+            <input type="checkbox" id="nintendo" name="plateformes[]" value="Nintendo">
+            <label for="nintendo">Nintendo</label>
+
+            <input type="checkbox" id="pc" name="plateformes[]" value="PC">
+            <label for="pc">PC</label>
+        </div>
+
+        <label for="url_cover_game">URL de la cover :</label>
+        <input type="url" id="url_cover_game" name="url_cover_game" placeholder="URL de la cover" required>
+
+        <label for="url_site_game">URL du site :</label>
+        <input type="url" id="url_site_game" name="url_site_game" placeholder="URL du site" required>
+
+        <button type="submit">Ajouter le jeu</button>
     </form>
 </div>
 <?php include 'footer.php'; ?>
