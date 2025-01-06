ccfd6a17d61b77b8310dc78af592e11c9e07d4ff
KuKaRaCHa-gg <daniilpub@gmail.com>
2025-01-06 21:37:30 +0100

    suite

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,13 +1,9 @@
 <?php
-require "./NavBar.php";
-require "./../Models/User.php";
-require "./../Models/LibraryModel.php";
-require "./../Views/HomeView.php";
 
 class HomeController {
     public function index() {
         session_start();
-
+        
         // Vérifier si l'utilisateur est connecté
         if (!isset($_SESSION['user_id'])) {
             header("Location: login_view.php");
@@ -15,14 +11,15 @@ class HomeController {
         }
 
         // Récupérer les données utilisateur et les jeux
+        include_once 'models/User.php';
+        include_once 'models/LibraryModel.php';
         $userModel = new User();
         $libraryModel = new LibraryModel();
-        
+
         $user = $userModel->getUserById($_SESSION['user_id']);
         $games = $libraryModel->getGamesByUser($_SESSION['user_id']);
 
-        // Passer les données à la vue
-        HomeView::render($user, $games);
+        // Charger la vue
+        require_once 'views/home_view.php';
     }
 }
-?>
