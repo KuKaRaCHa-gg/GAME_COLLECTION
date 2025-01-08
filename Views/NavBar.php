f3460b5f84d8fcaa456cec4c64724e0e530f40e1
MaxouHunter11 <maxbanuls@laposte.net>
2025-01-08 13:40:08 +0100

    essai fix navbar

--- Views/NavBar.php
+++ Views/NavBar.php
@@ -1,28 +1,28 @@
 <!DOCTYPE>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="../Assets/CSS/General.css">
-    <link rel="stylesheet" type="text/css" href="../Assets/CSS/NavBar.css">
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/General.css">
+    <link rel="stylesheet" type="text/css" href="Assets/CSS/NavBar.css">
 </head>
 <body>
 <div id="navabarFrame">
-    <img id="logo" src="../Assets/Images/Logo.png" alt="Logo"/>
+    <img id="logo" src="Assets/Images/Logo.png" alt="Logo"/>
     <div id="buttonStorage">
         <button type="button" id="buttonHome">
             MA BIBLIOTHÈQUE
-            <img src="./../Assets/Images/livres.png" alt="livres" id="livres" class="navimages">
+            <img src="Assets/Images/livres.png" alt="livres" id="livres" class="navimages">
         </button>
         <button type="button" id="buttonCollection">
             AJOUTER UN JEU
-            <img src="./../Assets/Images/manette.png" alt="manette" id="manette" class="navimages">
+            <img src="Assets/Images/manette.png" alt="manette" id="manette" class="navimages">
         </button>
         <button type="button" id="buttonSearch">
             CLASSEMENT
-            <img src="./../Assets/Images/couronne.png" alt="couronne" id="couronne" class="navimages">
+            <img src="Assets/Images/couronne.png" alt="couronne" id="couronne" class="navimages">
         </button>
         <button type="button" id="buttonProfile">
             PROFIL
-            <img src="./../Assets/Images/profil.png" alt="profil" id="profil" class="navimages">
+            <img src="Assets/Images/profil.png" alt="profil" id="profil" class="navimages">
         </button>
     </div>
 </div>
