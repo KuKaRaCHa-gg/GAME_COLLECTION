0a4ed69653a7457f735f6fc1edef7ebe30262a74
MattisGoom64 <schaefermattis64@gmail.com>
2025-01-08 21:56:13 +0100

    Mis a jour du CSS de profil

--- Models/User.php
+++ Models/User.php
@@ -69,61 +69,35 @@ function getUserToModif($pdo, $id)
     $query->execute();
     $row = $query->fetch(PDO::FETCH_ASSOC);
     echo '<br>';
-    echo '<form action="" method="post">';
+    echo '<form action="" method="post" class="formProfil">';
 
-    echo '<label for="nom_user">Nom</label><br>';
+    echo '<label for="nom_user">Nom :</label><br>';
     echo '<input type="text" name="nom_user" value="' . htmlspecialchars($row['nom_user']) . '"><br>';
 
-    echo '<label for="pren_user">Prénom</label><br>';
+    echo '<label for="pren_user">Prénom :</label><br>';
     echo '<input type="text" name="pren_user" value="' . htmlspecialchars($row['pren_user']) . '"><br>';
 
-    echo '<label for="mail_user">Email</label><br>';
+    echo '<label for="mail_user">Email :</label><br>';
     echo '<input type="text" name="mail_user" value="' . htmlspecialchars($row['mail_user']) . '"><br>'; 
 
-    echo '<label for="mdp_user">Mot de passe</label><br>';
+    echo '<label for="mdp_user">Mot de passe :</label><br>';
     echo '<input type="password" name="mdp" ><br>';
-    echo '<label for="mdp_user">Confirmer le mot de passe</label><br>';
+    echo '<label for="mdp_user">Confirmer le mot de passe :</label><br>';
     echo '<input type="password" name="mdp2" ><br>';
 
-    echo '<input type="submit" name="submit" value="Modifier"><br>';
-    echo '<input type="submit" name="submit" value="Supprimer mon compte"><br>';
-    echo '<input type="submit" name="submit" value="Se déconnecter"><br>';
+    echo '<input type="submit" name="submit" value="MODIFIER" class="boutonProfil"><br>';
+    echo '<input type="submit" name="submit" value="SUPPRIMER MON COMPTE" class="boutonProfil"><br>';
+    echo '<input type="submit" name="submit" value="SE DÉCONNECTER" class="boutonProfil"><br>';
     echo '</form>';
 
     if (isset($_POST['submit'])) {
-        if ($_POST['submit'] == 'Modifier') {
-            
-            if ($_POST['mdp'] == "") {
-                $sql = "UPDATE UTILISATEUR SET nom_user = :nom_user, pren_user = :pren_user, mail_user = :mail_user WHERE id_user = :id";
-                $query = $pdo->prepare($sql);
-                $query->bindParam(':nom_user', $_POST['nom_user'], PDO::PARAM_STR);
-                $query->bindParam(':pren_user', $_POST['pren_user'], PDO::PARAM_STR);
-                $query->bindParam(':mail_user', $_POST['mail_user'], PDO::PARAM_STR);
-                $query->bindParam(':id', $id, PDO::PARAM_INT);
-                $query->execute();
-                header("Location: index.php?action=profile");
-                exit();
-            } elseif ($_POST['mdp'] == $_POST['mdp2']) {
-                $mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
-                $query = $pdo->prepare("UPDATE UTILISATEUR SET nom_user = :nom_user, pren_user = :pren_user, mail_user = :mail_user, mdp_user = :mdp_user WHERE id_user = :id");
-                $query->bindParam(':nom_user', $_POST['nom_user'], PDO::PARAM_STR);
-                $query->bindParam(':pren_user', $_POST['pren_user'], PDO::PARAM_STR);
-                $query->bindParam(':mail_user', $_POST['mail_user'], PDO::PARAM_STR);
-                $query->bindParam(':mdp_user', $mdp, PDO::PARAM_STR);
-                $query->bindParam(':id', $id, PDO::PARAM_INT);
-                $query->execute();
-                header("Location: index.php?action=profile");
-                exit();
-            } else {
-                echo 'Les mots de passe ne correspondent pas';
-            }
-        } elseif ($_POST['submit'] == 'Supprimer mon compte') {
-            $query = $pdo->prepare("DELETE FROM UTILISATEUR WHERE id_user = :id");
-            $query->bindParam(':id', $id, PDO::PARAM_INT);
-            $query->execute();
-            header("Location: index.php?action=login");
+        if ($_POST['submit'] == 'MODIFIER') {
+            gestionMDP($pdo, $id, $_POST['mdp'], $_POST['mdp2'], $_POST['nom_user'], $_POST['pren_user'], $_POST['mail_user']);
+        } 
+        elseif ($_POST['submit'] == 'SUPPRIMER MON COMPTE') {
+            deleteUser($pdo, $id);
             exit();
-        } elseif ($_POST['submit'] == 'Se déconnecter') {
+        } elseif ($_POST['submit'] == 'SE DÉCONNECTER') {
             logout();
         }
     }
@@ -134,4 +108,44 @@ function logout()
     session_destroy();
     header("Location: index.php?action=login");
     exit();
+}
+
+function deleteUser($pdo, $id)
+{
+    $query = $pdo->prepare("DELETE FROM UTILISATEUR WHERE id_user = :id");
+    $query->bindParam(':id', $id, PDO::PARAM_INT);
+    $query->execute();
+    header("Location: index.php?action=login");
+    exit();
+}
+
+function gestionMDP($pdo, $id, $mdp, $mdp2, $nom, $prenom, $email)
+{
+    
+    if ($mdp == "") {
+        $sql = "UPDATE UTILISATEUR SET nom_user = :nom_user, pren_user = :pren_user, mail_user = :mail_user WHERE id_user = :id";
+        $query = $pdo->prepare($sql);
+        $query->bindParam(':nom_user', $nom, PDO::PARAM_STR);
+        $query->bindParam(':pren_user', $prenom, PDO::PARAM_STR);
+        $query->bindParam(':mail_user', $email, PDO::PARAM_STR);
+        $query->bindParam(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+        $status = 'modif';
+        header("Location: index.php?action=profile");
+        exit();
+    } elseif ($mdp == $mdp2) {
+        $mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+        $query = $pdo->prepare("UPDATE UTILISATEUR SET nom_user = :nom_user, pren_user = :pren_user, mail_user = :mail_user, mdp_user = :mdp_user WHERE id_user = :id");
+        $query->bindParam(':nom_user', $nom, PDO::PARAM_STR);
+        $query->bindParam(':pren_user', $prenom, PDO::PARAM_STR);
+        $query->bindParam(':mail_user', $email, PDO::PARAM_STR);
+        $query->bindParam(':mdp_user', $mdp, PDO::PARAM_STR);
+        $query->bindParam(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+        $status = 'modif';
+        header("Location: index.php?action=profile");
+        exit();
+    } else {
+        echo 'Les mots de passe ne correspondent pas';
+    }
 }
--- Views/profile_view.php
+++ Views/profile_view.php
@@ -10,44 +10,31 @@ $pdo = connexion(); ?>
     <title>Document</title>
 </head>
 <body>
-<br>
-
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
     <div class="profilContainer">
     <div class="profil">
-    <h2>Mon Profil</h2>
-    <br>
-    <br>
-
+    <h1 class="titreProfil">Mon Profil</h1>
 
     <?php 
+    $status = '';
     if (isset($_POST['submit'])) {
-        if ($_POST['submit'] == 'Modifier mon profile') {
+        if ($_POST['submit'] == 'MODIFIER MON PROFILE') {
             getUserToModif($pdo, $_SESSION['user_id']);
-        } elseif ($_POST['submit'] == 'Supprimer mon compte') {
-            header('Location: index.php?action=deleteProfile');
-        } elseif ($_POST['submit'] == 'Se déconnecter') {
-            session_destroy();
-            header('Location: index.php?action=logout');
-            exit();
+        } elseif ($_POST['submit'] == 'SUPPRIMER MON COMPTE') {
+            deleteUser($pdo, $_SESSION['user_id']);
+        } elseif ($_POST['submit'] == 'SE DÉCONNECTER') {
+            logout();
         }
-    } else {
+    } elseif (empty($_POST['submit']) || $status == 'modif') {
     $users = getUser($pdo, $_SESSION['user_id']); ?>
-    <br>
 
-    <p> Nom : <?php echo htmlspecialchars($users['nom_user']); ?></p>
-    <p> Prénom : <?php echo htmlspecialchars($users['pren_user']); ?></p>
-    <p> Email : <?php echo htmlspecialchars($users['mail_user']); ?></p>
+    <p class="profilTexte"> Nom : <?php echo htmlspecialchars($users['nom_user']); ?></p>
+    <p class="profilTexte"> Prénom : <?php echo htmlspecialchars($users['pren_user']); ?></p>
+    <p class="profilTexte"> Email : <?php echo htmlspecialchars($users['mail_user']); ?></p>
 
 <form class="formProfil" action="" method="post">
-    <input type="submit" name="submit" value="Modifier mon profile"> <br>
-    <input type="submit" name="submit" value="Supprimer mon compte"> <br>
-    <input type="submit" name="submit" value="Se déconnecter"> <br>
+    <input type="submit" name="submit" value="MODIFIER MON PROFILE" class="boutonProfil"> <br>
+    <input type="submit" name="submit" value="SUPPRIMER MON COMPTE" class="boutonProfil"> <br>
+    <input type="submit" name="submit" value="SE DÉCONNECTER" class="boutonProfil"> <br>
 </form>
 
     <?php }
