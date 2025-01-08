a9c3bd556542b830802ff5b0489261f0c7d880cd
MaxouHunter11 <maxbanuls@laposte.net>
2025-01-08 14:01:49 +0100

    Revert "test déplacement"
    This reverts commit 4f2970ecc9f6c605f6e643db0cb0eb1ffedf9f12.

--- Views/NavBar.php
+++ Views/NavBar.php
@@ -8,22 +8,22 @@
 <div id="navabarFrame">
     <img id="logo" src="Assets/Images/Logo.png" alt="Logo"/>
     <div id="buttonStorage">
-        <a  id="buttonHome" href="">
+        <button type="button" id="buttonHome">
             MA BIBLIOTHÈQUE
             <img src="Assets/Images/livres.png" alt="livres" id="livres" class="navimages">
-        </a>
-        <a  id="buttonCollection" href="">
+        </button>
+        <button type="button" id="buttonCollection">
             AJOUTER UN JEU
             <img src="Assets/Images/manette.png" alt="manette" id="manette" class="navimages">
-        </a>
-        <a  id="buttonSearch" href="Views/ranking_view.php">
+        </button>
+        <button type="button" id="buttonSearch">
             CLASSEMENT
             <img src="Assets/Images/couronne.png" alt="couronne" id="couronne" class="navimages">
-        </a>
-        <a  id="buttonProfile" href="">
+        </button>
+        <button type="button" id="buttonProfile">
             PROFIL
             <img src="Assets/Images/profil.png" alt="profil" id="profil" class="navimages">
-        </a>
+        </button>
     </div>
 </div>
 <div id="navbarCollision">
