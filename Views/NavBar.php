b9b46956d112e6addd1d8fac775f3656ce48d06c
MattisGoom64 <schaefermattis64@gmail.com>
2025-01-08 19:55:38 +0100

    Mise en place des lien de navigation

--- Views/NavBar.php
+++ Views/NavBar.php
@@ -9,19 +9,19 @@
     <img id="logo" src="Assets/Images/Logo.png" alt="Logo"/>
     <div id="buttonStorage">
         <button type="button" id="buttonHome">
-            MA BIBLIOTHÈQUE
+        <a href="index.php?action=home">MA BIBLIOTHÈQUE</a>
             <img src="Assets/Images/livres.png" alt="livres" id="livres" class="navimages">
         </button>
         <button type="button" id="buttonCollection">
-            AJOUTER UN JEU
+            <a href="index.php?action=add_game">AJOUTER UN JEU</a>
             <img src="Assets/Images/manette.png" alt="manette" id="manette" class="navimages">
         </button>
         <button type="button" id="buttonSearch">
-            CLASSEMENT
+        <a href="index.php?action=ranking">CLASSEMENT</a>
             <img src="Assets/Images/couronne.png" alt="couronne" id="couronne" class="navimages">
         </button>
         <button type="button" id="buttonProfile">
-            PROFIL
+        <a href="index.php?action=profile">PROFIL</a>
             <img src="Assets/Images/profil.png" alt="profil" id="profil" class="navimages">
         </button>
     </div>
