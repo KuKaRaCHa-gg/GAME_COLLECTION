5dea91fb68d5c1bed18909ea4d856548de492ab7
MattisGoom64 <schaefermattis64@gmail.com>
2025-01-09 18:34:41 +0100

    Page ajout à la bibliothéque relié avec la page d'ajout de jeu dans la base de donnée

--- Views/addLibraryView.php
+++ Views/addLibraryView.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,11 +23,28 @@
 
 
 <h2 class="resultatAffiche"> Resultats de la recherche </h2>
+<?php 
+if (isset($_POST['add_game'])) {
+    if (verifyGame($pdo, $_SESSION['user_id'], $_POST['id_game']) == true) {
+        ?>
+        <p class="messageAjout">Le jeu <?php echo htmlspecialchars($_POST['nom_game']) ?> est déjà dans votre bibliothèque </p>
+        <?php 
+    } else {
+        addGameToLibrary($pdo, $_SESSION['user_id'], $_POST['id_game']);
+        ?>
+        <p class="messageAjout">Le jeu <?php echo htmlspecialchars($_POST['nom_game']) ?> a bien été ajouté à votre bibliothèque </p>
+        <?php 
+    }
+}
+?>
 </div>
 </div>
 <?php 
-if (isset($_POST['search_action'])) {
+if (isset($_POST['search_action']) && $_POST['search_game'] != '') {
     $games = searchGame($pdo, $_POST['search_game']);
+    if (empty($games)) {
+        header("Location: index.php?action=add_game");
+    }
 } else {
     $games = getAllGames($pdo);
 }
@@ -40,22 +59,13 @@ foreach ($games as $game) {
     <img src= <?php echo htmlspecialchars($game['url_cover_game']) ?> alt="jeu" class="gameImage"/>
     <form action="" method="POST" class="formDefaut">
     <input type="hidden" name="id_game" value=" <?php echo $game['id_game'] ?> ">
+    <input type="hidden" name="nom_game" value=" <?php echo $game['nom_game'] ?> ">
     <button type="submit" name="add_game" value="add_game" class="boutonAjout">AJOUTER A LA BIBLIOTHÈQUE</button>
     </form>
     </div>
     <?php
 
 }
-
-if (isset($_POST['add_game'])) {
-    if (verifyGame($pdo, $_SESSION['user_id'], $_POST['id_game']) == true) {
-        echo 'Le jeu est déjà dans votre bibliothèque';
-    } else {
-        addGameToLibrary($pdo, $_SESSION['user_id'], $_POST['id_game']);
-        echo 'Le jeu a bien été ajouté à votre bibliothèque';
-    }
-}
-
 
 ?>
 </div>
